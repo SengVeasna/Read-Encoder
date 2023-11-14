261e30b775ec977b16c12b25cdd674869ee0519c
SengVeasna <148682359+SengVeasna@users.noreply.github.com>
2023-11-14 11:43:27 +0700

    Add files via upload

---
+++ Src/main.c
@@ -0,0 +1,306 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.c
+  * @brief          : Main program body
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2023 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "tim.h"
+#include "gpio.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include "stdlib.h"
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+#define r 0.03
+#define pi 3.14
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+
+/* USER CODE BEGIN PV */
+float sampleTime=1000.00;
+float CPR=1200.00;
+
+
+float Enc_count[1];
+float d_prev=0.00;
+float d_current=0.00;
+float deg=0.00;
+
+
+volatile float nowA[4];
+volatile float nowB[4];
+volatile float dir[4];
+float rev;
+float diff;
+float c;
+float new_count;
+float speed;
+float lastA[4];
+float lastB[4];
+float cnt[4];
+float encoder(int i)
+{
+	if (nowA[i] != lastA[i])
+	{
+		lastA[i] = nowA[i];
+		if (lastA[i] == 0)
+		{
+			if (nowB[i] == 0)
+			{
+				dir[i] = 0;
+				cnt[i]--;
+			}
+			else
+			{
+				dir[i] = 1;
+				cnt[i]++;
+			}
+		}
+		else
+		{
+			if (nowB[i] == 1)
+			{
+				dir[i] = 0;
+				cnt[i]--;
+			}
+			else
+			{
+				dir[i] = 1;
+				cnt[i]++;
+			}
+		}
+	}
+	if (nowB[i] != lastB[i])
+	{
+		lastB[i] = nowB[i];
+		if (lastB[i] == 0)
+		{
+			if (nowA[i] == 1)
+			{
+				dir[i] = 0;
+				cnt[i]--;
+			}
+			else
+			{
+				dir[i] = 1;
+				cnt[i]++;
+			}
+		}
+		else
+		{
+			if (nowA[i] == 0)
+			{
+				dir[i] = 0;
+				cnt[i]--;
+			}
+			else
+			{
+				dir[i] = 1;
+				cnt[i]++;
+			}
+		}
+	}
+	return cnt[i];
+}
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+void SystemClock_Config(void);
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/**
+  * @brief  The application entry point.
+  * @retval int
+  */
+int main(void)
+{
+  /* USER CODE BEGIN 1 */
+
+  /* USER CODE END 1 */
+
+  /* MCU Configuration--------------------------------------------------------*/
+
+  /* Reset of all peripherals, Initializes the Flash interface and the Systick. */
+  HAL_Init();
+
+  /* USER CODE BEGIN Init */
+
+  /* USER CODE END Init */
+
+  /* Configure the system clock */
+  SystemClock_Config();
+
+  /* USER CODE BEGIN SysInit */
+
+  /* USER CODE END SysInit */
+
+  /* Initialize all configured peripherals */
+  MX_GPIO_Init();
+  MX_TIM2_Init();
+  /* USER CODE BEGIN 2 */
+  HAL_TIM_Base_Start_IT(&htim2);
+
+  /* USER CODE END 2 */
+
+  /* Infinite loop */
+  /* USER CODE BEGIN WHILE */
+  while (1)
+  {
+    /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
+	  HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_13);
+	  HAL_Delay(250);
+  }
+  /* USER CODE END 3 */
+}
+
+/**
+  * @brief System Clock Configuration
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
+
+  /** Initializes the RCC Oscillators according to the specified parameters
+  * in the RCC_OscInitTypeDef structure.
+  */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_ON;
+  RCC_OscInitStruct.HSEPredivValue = RCC_HSE_PREDIV_DIV1;
+  RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLMUL = RCC_PLL_MUL9;
+  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /** Initializes the CPU, AHB and APB buses clocks
+  */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
+                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV2;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+
+  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/* USER CODE BEGIN 4 */
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+  {
+	  if(GPIO_Pin == GPIO_PIN_8|| GPIO_PIN_9)
+	  {
+		  nowA[0]=HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_8);
+		  nowB[0]=HAL_GPIO_ReadPin(GPIOA, GPIO_PIN_9);
+		  Enc_count[0] = encoder(0);
+	  }
+  }
+
+  void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim)
+  {
+	  if(htim->Instance == TIM2)
+	  {
+		  new_count = Enc_count[0];
+		  rev = new_count/CPR;
+		  c = pi*r;
+		  d_current = c*rev;
+		  if(d_current >=0)
+		  {
+			  d_current = d_current;
+		  }
+		  else
+		  {
+			  d_current = 1*d_current;
+		  }
+		  deg = 2*180*rev;
+		  if(deg >= 360)
+		  {
+			  deg = 360-deg;
+		  }
+		  else
+		  {
+			  deg = deg;
+		  }
+		  diff = d_current - d_prev;
+		  speed = abs(diff)*1000.0f/sampleTime;
+		  d_prev = d_current;
+	  }
+  }
+/* USER CODE END 4 */
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @retval None
+  */
+void Error_Handler(void)
+{
+  /* USER CODE BEGIN Error_Handler_Debug */
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
+  /* USER CODE END Error_Handler_Debug */
+}
+
+#ifdef  USE_FULL_ASSERT
+/**
+  * @brief  Reports the name of the source file and the source line number
+  *         where the assert_param error has occurred.
+  * @param  file: pointer to the source file name
+  * @param  line: assert_param error line source number
+  * @retval None
+  */
+void assert_failed(uint8_t *file, uint32_t line)
+{
+  /* USER CODE BEGIN 6 */
+  /* User can add his own implementation to report the file name and line number,
+     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+  /* USER CODE END 6 */
+}
+#endif /* USE_FULL_ASSERT */
